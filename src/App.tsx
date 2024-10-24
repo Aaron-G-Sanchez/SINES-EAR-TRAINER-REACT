7a89790c9787077c0aa1c0ff87863e20a0516097
Aaron Sanchez <saarong12@gmail.com>
2024-10-23 23:36:46 -0500

    Added JSON file to hold all frequencies for each level.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
+import {beginner} from'./frequencies.json'
 
 export const App = () => {
 
   return (
     <>
      <div>Hello World!</div>
+     {beginner.map((freq, idx) => <p key={idx}>{freq}</p>)}
     </>
   )
 }
